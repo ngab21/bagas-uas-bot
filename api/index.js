af89c0719f20163d880e02e21604dc386eb2ccf3
ngab21 <84616551+ngab21@users.noreply.github.com>
2021-07-16 16:18:42 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1779091825:AAHg20dEekOkD0WwCHcl9vBvdHmpa89SyRU'
+const token = '1857112511:AAFOWsjLmSfnGf_znvFUoz4YZFHMwZRmk3c'
 const bot = new TelegramBot(token, {polling: true});
 
 
